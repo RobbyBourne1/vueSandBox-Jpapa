974142a12c70e99308de3abc17d2f53fed1fcc28
robbybourne1 <robbybourne1@gmail.com>
2019-09-09 17:23:03 -0400

    initial commit

---
+++ 07-accessing-data/begin/vue-heroes/src/shared/data.js
@@ -0,0 +1,37 @@
+import { format } from 'date-fns';
+import { inputDateFormat } from './constants';
+import * as axios from 'axios'
+import { API } from './config'
+
+const getHeros = async function () {
+  try {
+    const response = await axios.get(`${API}/heroes.json`)
+    let data = parseList(response)
+    const heroes = data.map(m => {
+      m.originDate = format(m.originDate, inputDateFormat)
+      return m
+    })
+    return heroes
+  } catch (error) {
+    console.error(error)
+    return []
+  }
+}
+
+const parseList = response => {
+  if (response.status != 200) {
+    throw Error(response.message)
+  }
+  if (!response.data) {
+    return []
+  }
+  let list = response.data
+  if (typeof list != 'object') {
+    list = []
+  }
+  return list
+}
+
+export const data = {
+  getHeros,
+};
---
+++ 08-routing/begin/vue-heroes/src/router.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import PageNotfound from './views/page-not-found.vue'
+
+Vue.use(Router)
+const parseProps = r => ({
+  id: parseInt(r.params.id)
+})
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      redirect: '/heroes'
+    },
+    {
+      path: '/heroes',
+      name: 'heroes',
+      component: () => import(/* webpackChunkName: "bundle-heroes" */ './views/heroes.vue'),
+    },
+    {
+      path: '/heroes/:id',
+      name: 'hero-detail',
+      component: () => import(/* webpackChunkName: "bundle-heroes" */ './views/hero-detail.vue'),
+      props: parseProps,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "bundle-about" */ './views/about.vue')
+    },
+    {
+      path: '*',
+      component: PageNotfound,
+    }
+  ]
+})
